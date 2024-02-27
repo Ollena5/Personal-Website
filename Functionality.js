ac5ace9a6c22a0b3b635f60b84ec5fbb9951b593
Ollena5 <125416908+Ollena5@users.noreply.github.com>
2024-02-27 17:33:23 +0000

    color

--- Functionality.js
+++ Functionality.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
+    const menuToggle = document.querySelector('.hamburger-menu');
     const menu = document.querySelector('.menu');
 
     menuToggle.addEventListener('click', function() {
